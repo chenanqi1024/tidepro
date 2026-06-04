f220e47a04b14f0b670be5f6e500c2e6dca630a5
chenanqi1024 <chenanqi1024@gmail.com>
2026-06-04 23:13:02 +0800

    Initial Commit

---
+++ tidepro/tideproApp.swift
@@ -0,0 +1,17 @@
+//
+//  tideproApp.swift
+//  tidepro
+//
+//  Created by chenanqi on 2026/6/4.
+//
+
+import SwiftUI
+
+@main
+struct tideproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
